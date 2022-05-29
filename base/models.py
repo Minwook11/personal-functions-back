21d65d28d1fb2328bb0f133a803c29b11dbec62d
Minwook11 <alsdnr4874@gmail.com>
2022-05-29 20:55:39 +0900

    [Add] Initialize User model  - Common form about User domain

--- base/models.py
+++ base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    account    = models.EmailField(max_length=128, unique=True)
+    password   = models.CharField(max_length=256)
+    name       = models.CharField(max_length=128, null=True)
+    birthday   = models.DateTimeField(null=True)
+    department = models.CharField(max_length=128, null=True)
+    cellphone  = models.CharField(max_length=64, null=True)
+    timestamp  = models.DateTimeField(auto_now_add=True)
+    is_active  = models.BooleanField(default=False)
+    is_admin   = models.BooleanField(default=False)
